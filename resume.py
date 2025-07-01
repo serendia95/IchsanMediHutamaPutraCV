cc4f8ca2b39fa4c6386fcf04408e8b6b7eebccee
serendia95 <57470438+serendia95@users.noreply.github.com>
2025-07-01 04:01:52 +0000

    update

--- resume.py
+++ resume.py
@@ -8,15 +8,3 @@ with open("style.css") as f:
 with open("layout.html", 'r') as f:
     html_content = f.read()
     st.markdown(html_content, unsafe_allow_html=True)
-
-# Sidebar Contact Info
-with st.sidebar:
-    st.title("📬 Contact")
-    st.markdown("""
-    📞 +62 821-1411-4195  
-    📧 ichsanmedi95@gmail.com  
-    📧 ichsanmedi_v@ymail.com  
-    📍 Lippo Karawaci, Tangerang, Banten  
-    [LinkedIn](https://www.linkedin.com/in/ichsan-medi-hutama-putra/)  
-    [GitHub](https://github.com/serendia95)  
-    """)
